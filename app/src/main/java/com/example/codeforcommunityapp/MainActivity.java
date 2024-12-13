ebe6c464b9334e4682ed7821412a1182ba523617
Jonathan Siew <jonathansiew.2004@gmail.com>
2024-12-14 02:00:01 +0800

    Adding Function when Home Logo is Pressed

--- app/src/main/java/com/example/codeforcommunityapp/MainActivity.java
+++ app/src/main/java/com/example/codeforcommunityapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.codeforcommunityapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -9,22 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageButton btnHome = findViewById(R.id.homelogo);
+        if (btnHome != null) {
+            btnHome.setOnClickListener(v -> {
+                try {
+                    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+                    navController.navigate(R.id.homePage);
+                } catch (IllegalArgumentException e) {
+                    Log.e("Navigation", "Unable to find NavController", e);
+                }
+            });
+        } else {
+            Log.e("UI", "Home button not found in layout");
+        }
     }
-
-    ImageButton btnHome;
-
-
-
 }
